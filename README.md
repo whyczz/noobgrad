ee0fec4cf94f8c043dc5a298b968add2f0d70f10
whycz <whyczz@gmail.com>
2025-12-17 12:56:31 +0100

    0.3 add reflected operators and syntatic sugar to unwrap ints/floats

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 
 - [0.1] Add a Scalar with a value, repr
 - [0.2] Add operations (add, mul, exp) to Scalar
+- [0.3] Reflected operators, convenience unwrapping ints or floats
+- [0.4] Trace the history of operators, graph it
+- [0.5] Add backwards, gradient accumulation e.g. sums
+- [0.6] Viz backwards pass with a gif
+- [0.7] Neural Net
+- [0.8] Layer, MLP
+- [0.9] Training loop
+- [1.0] Training epoch 
--- ascalar.py
+++ ascalar.py
@@ -3,6 +3,8 @@ Karpathy's engine.py, or pytorch's ATen
 """
 import math
 
+type ScalarLike = "Scalar" | int | float
+
 class Scalar:
     def __init__(self, value: int | float):
         self.value = value
@@ -11,25 +13,59 @@ class Scalar:
         return math.isclose(self.value, other.value)
 
     # Operators
-    def __add__(self, other: "Scalar") -> "Scalar":
+    def __add__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
         return Scalar(value=self.value + other.value)
 
-    def __mul__(self, other: "Scalar") -> "Scalar":
+    def __mul__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
         return Scalar(value=self.value * other.value)
     
-    def __pow__(self, other: int | float) -> "Scalar":
-        return Scalar(value=self.value ** other)
+    def __pow__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return Scalar(value=self.value ** other.value)
 
     def __neg__(self):
         return Scalar(value=-self.value)
 
-    def __truediv__(self, other: "Scalar") -> "Scalar":
+    def __truediv__(self, other: ScalarLike) -> "Scalar":
         return Scalar(value=self.value * other.value**-1)
     
-    def __sub__(self, other: "Scalar") -> "Scalar":
+    def __sub__(self, other: ScalarLike) -> "Scalar":
         return self + (-other)
 
     # Reflected Operators (e.g. other + self)
+    def __radd__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return other + self
+
+    def __rsub__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return other - self
+
+    def __rmul__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return other * self
+
+    def __rtruediv__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return other / self 
+
+    def __rpow__(self, other: ScalarLike) -> "Scalar":
+        if not isinstance(other, Scalar):
+            other = Scalar.wrap_num(other)
+        return other ** self
 
     def __repr__(self):
         return f"Scalar(value={self.value})"
+
+    @staticmethod
+    def wrap_num(num: int | float) -> "Scalar":
+        return Scalar(value=num)
