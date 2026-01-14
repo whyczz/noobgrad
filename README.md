7f726a257b637f6e3c4b898073d8b7f519f1edbf
whycz <whyczz@gmail.com>
2026-01-13 23:45:31 -0500

    0.6.1 add Neuron

--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 - [0.4] Trace the history of operators, graph it
 - [0.5] Add backwards, gradient accumulation e.g. sums
 - [0.5.1] Add tanh() and exp() forwards, backwards 
-- [0.7] Neural Net
-- [0.8] Layer, MLP
-- [0.9] Training loop
-- [1.0] Training epoch 
+- [0.6] Neural Net
+- [0.6.1] Neuron
+- [0.6.2] Layer
+- [0.6.3] MLP
+- [0.7] Training loop
+- [0.8] Training epoch 
--- nn.py
+++ nn.py
@@ -2,3 +2,36 @@
 from torch.nn import ?
 from noobgrad.nn import ?
 """
+
+import random
+import math
+from typing import Iterable 
+from functools import reduce
+
+from ascalar import Scalar, ScalarLike
+
+class Neuron:
+    def __init__(self, n_in: int):  # n_in = num_inputs
+        self.w = [Scalar(random.uniform(-1, 1)) for _ in range(n_in)] # weights
+        self.b = Scalar(random.uniform(-1, 1)) # bias
+    
+    def forward(self, x: Iterable[ScalarLike]) -> Iterable[ScalarLike]:  # x = inputs
+        assert len(x) == len(self.w), f"mismatched num_inputs {len(self.w)=} and input length {len(x)=}"
+
+        dot_prod = 0
+        for w_, x_ in zip(self.w, x):
+            dot_prod += w_*x_
+        
+        linear = dot_prod + self.b
+        output = linear.tanh()
+        return output
+
+    def parameters(self) -> Iterable[Scalar]:
+        return [*self.w, self.b] 
+    
+    def __call__(self, x: Iterable[Scalar]) -> ScalarLike:
+        return self.forward(x)
+
+    def __str__(self) -> str:
+        return f"Neuron({self.w=}, {self.b=})"
+
